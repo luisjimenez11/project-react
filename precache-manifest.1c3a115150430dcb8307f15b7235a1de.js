ef2a2427e341e45db906deb5a7f1bf6a6e266357
Luis Jimenez <luis.jimenez11@gmail.com>
2019-06-04 02:11:09 -0400

    Updates

---
+++ precache-manifest.1c3a115150430dcb8307f15b7235a1de.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6766e799fe54238ea5b5f8101fe0db78",
+    "url": "/project-react/index.html"
+  },
+  {
+    "revision": "29ec52bc87ec01163743",
+    "url": "/project-react/static/css/4.6942973f.chunk.css"
+  },
+  {
+    "revision": "3f94fb24f3f2984c998e",
+    "url": "/project-react/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "1e5fbf28b0aa7645732b",
+    "url": "/project-react/static/js/0.f47b2233.chunk.js"
+  },
+  {
+    "revision": "1cd90e5d002d9324d77d",
+    "url": "/project-react/static/js/3.5eb9c39a.chunk.js"
+  },
+  {
+    "revision": "29ec52bc87ec01163743",
+    "url": "/project-react/static/js/4.9e612531.chunk.js"
+  },
+  {
+    "revision": "44350fd3861da3410b91",
+    "url": "/project-react/static/js/5.4df4dc83.chunk.js"
+  },
+  {
+    "revision": "7a0e0881e6c7abf7d9ba",
+    "url": "/project-react/static/js/6.a790cab4.chunk.js"
+  },
+  {
+    "revision": "3f94fb24f3f2984c998e",
+    "url": "/project-react/static/js/main.ff25ef5a.chunk.js"
+  },
+  {
+    "revision": "1ae15de7bf7e233239aa",
+    "url": "/project-react/static/js/runtime~main.bdba6aaf.js"
+  }
+]);
--- src/pages/RegisterPage/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import RegisterContainer from '../../containers/RegisterContainer'
-
-const RegisterPage = () => (
-  <RegisterContainer />
-)
-export default RegisterPage
